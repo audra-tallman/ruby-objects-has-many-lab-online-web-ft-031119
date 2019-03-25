d5de7331b650a7ee45a8f2b76b6a0f63cc9e3ac5
Audra Tallman <audi.tallman@gmail.com>
2019-03-25 22:34:50 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,1 +1,26 @@
 class Post
+  attr_accessor :title, :author
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title 
+    @@all << self
+  end 
+  
+  def save
+    @@all << self  
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def author_name
+    if self.author.name == author.name
+      author.name
+    else 
+      nil 
+    end 
+  end 
+end 
