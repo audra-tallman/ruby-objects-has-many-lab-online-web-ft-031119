51698f8775e00a1d090dc163596c65165011bbad
Audra Tallman <audi.tallman@gmail.com>
2019-03-25 22:24:16 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,7 +12,6 @@ def post(post)
   post.author = self
 end 
 
-
 def add_post(title)
   post.author = self
 
@@ -21,10 +20,8 @@ def add_post_by_title(title)
   post.author = self
 end
 
-
 def self.post_count
   Post.all.count
 end 
 
-end
 end 
